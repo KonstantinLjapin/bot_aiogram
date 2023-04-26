ab60d2f001d4088b32bc3010f4c0fc60f811ea4b
konstantin_liapin <lajpinkonstantin@gmail.com>
2023-04-26 17:18:19 +0300

    BOT_TOKEN, gitignore

--- iogram_bot/base.py
+++ iogram_bot/base.py
@@ -1,19 +1,14 @@
 import logging
 import os
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message
 from dotenv import load_dotenv, find_dotenv
 if not find_dotenv():
     exit('Переменные окружения не загружены т.к отсутствует файл .env')
 else:
     load_dotenv()
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-
-# Initialize bot and dispatcher
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
+BOT_TOKEN: str = os.environ.get('BOT_TOKEN')
 
 if __name__ == '__main__':
-    pass
+    print(BOT_TOKEN)
