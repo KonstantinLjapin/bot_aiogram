70bbfe611a63e65618b0c3751beb80763b44ee9a
konstantin_liapin <lajpinkonstantin@gmail.com>
2023-05-01 11:31:44 +0300

    add gitignor

--- iogram_bot/base.py
+++ iogram_bot/base.py
@@ -18,16 +18,9 @@ dp: Dispatcher = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
+@dp.message(Command(commands=["start", 'help']))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
-
-
-# Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
-async def process_help_command(message: Message):
-    await message.answer('Напиши мне что-нибудь и в ответ '
-                         'я пришлю тебе твое сообщение')
 
 
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
@@ -35,6 +28,7 @@ async def process_help_command(message: Message):
 @dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
+    print(message.json(indent=4, exclude_none=True))
 
 if __name__ == '__main__':
     dp.run_polling(bot)
