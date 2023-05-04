6a42ef9a902fef147e6b40ea2f84c8c32bb67620
konstantin_liapin <lajpinkonstantin@gmail.com>
2023-05-04 12:59:09 +0300

    add bot_log

--- iogram_bot/handlers/call.py
+++ iogram_bot/handlers/call.py
@@ -2,13 +2,14 @@ from ..config_data.config import bot_dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message, ContentType
 from aiogram import F
-import logging
+from ..loggin import log
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
 @bot_dispatcher.message(Command(commands=["start", 'help']))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+    log.bot_log(message.message_id)
 
 
 # Этот хэндлер будет срабатывать на тип контента "voice", "video" или "text"
@@ -19,11 +20,14 @@ async def process_start_command(message: Message):
                                             ContentType.PHOTO}))
 async def process_send_vovite(message: Message):
     await message.answer(text='Вы прислали контент который пока не поддерживается')
+    log.bot_log(message.message_id)
 
 
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
-# кроме команд "/start" и "/help"
 @bot_dispatcher.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
-    print(message.json(indent=4, exclude_none=True))
+    log.bot_log(message.message_id)
+
+
+if __name__ == '__main__':
+    pass
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 from iogram_bot.config_data.config import bot_dispatcher, bot
-
 if __name__ == '__main__':
     bot_dispatcher.run_polling(bot)
